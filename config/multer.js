1de8252c14fbab01670755296223d927bf5cfa35
Alvaro-Siqueira-Silva <84384180+Alvaro-Siqueira-Silva@users.noreply.github.com>
2022-04-06 12:07:56 -0300

    Update multer.js

--- config/multer.js
+++ config/multer.js
@@ -13,9 +13,9 @@ var upload = multer(
     {
         storage:armazenamento, limits:{fileSize:100000},
         fileFilter:(req,file,cb)=>{
-            if(file.mimetype == "images/png" ||
-            file.mimetype == "images/jpg" ||
-            file.mimetype == "images/jpeg"){
+            if(file.mimetype == "image/png" ||
+            file.mimetype == "image/jpg" ||
+            file.mimetype == "image/jpeg"){
                 cb(null,true)
             }else{
                 cb(null,false)
